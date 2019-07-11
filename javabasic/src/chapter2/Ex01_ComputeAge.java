e060ac865e8447d75f93b8c20c421334c23ef5dc
rabbit-hki007 <52555462+rabbit-hki007@users.noreply.github.com>
2019-07-11 17:12:26 +0900

    Initial commit

---
+++ javabasic/src/chapter2/Ex01_ComputeAge.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Ex01_ComputeAge {
+
+	public static void main(String[] args) {
+		String name = "";
+		int year = 0;
+		int age = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		// 키보드를 통한 입력
+		System.out.println("이름을 입력해 주세요 : ");
+		name = sc.next();
+		
+        System.out.println("생년을 입력해 주세요 : ");
+		year = sc.nextInt();
+		
+		// 나이 계산
+		age = 2019 - year;
+		// 화면상의 출력 
+		System.out.println(name + "님의 나이는" + age + "세 입니다");
+	} // end of main()
+
+} // end of class
